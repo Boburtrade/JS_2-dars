7e40c376f04a6f2080cb3755f4cbaa0b38da39b7
Maxmudjonov Boburjon <boburmahmudjonov484@gmail.com>
2023-06-01 21:53:31 +0500

    Bu JS 2-dars kodlari
    Yuklandi

---
+++ Jc/main.js
@@ -0,0 +1,62 @@
+/*
+let a = 44;
+let b = 43;
+
+if (a >= b){
+    console.log(" a qiymati 44dan katta yoki teng");
+}else{
+    console.log("aning qiymati korsatilgan sondan katta yoki teng emas!");
+}*/
+let a =10;
+let b =46;
+let c =50;
+let d =55;
+
+/*
+if (a == 10 && b == 46 && c == 50 && d == 55){
+    console.log("shartlarni hammasi bajarilsa, togri ishlaydi");
+}
+else{
+    console.log("Shartlarning birortasi hato bolsa ishlamaydi");
+}*/
+/*
+if (a == 1 || b == 4 || c == 5 || d == 5){
+    console.log("Shartni kamida 1 tasi bajarildi");
+}
+else{
+    console.log("shartlardan hammasi bajarilmadi");
+}*/
+ /* data types: undefined,boolean,object */
+/*
+let soz = "Text";
+let son = "45";
+let matn;
+let rost = 45==45?true:false;
+*/
+
+
+/*
+if (45 == 45){
+    document.write(true)
+}
+else{
+    document.write(false)
+}*/
+
+let arrayObject = [45,78,"text","matn",true,false];
+
+
+/*let oddiyObject = {ismi: "Navroz", familiyasi: "Malikov", yoshi:17,talabami: "ypq"};
+let talabami = "Yoq, talabama emas";
+if (oddiyObject.yoshi > 17){
+    talabami = "-Ha, talaba"
+}
+
+console.log("-Navroz talabami?" +talabami);*/
+
+let oddiyObject = {}
+let poytaxti = "-Toshkent, toshkent emas"
+if (oddiyObject.aholisi > 36){
+    poytaxti ="-ha, Toshkent"
+}
+console.log("-Uzbekistan poytaxti?" +poytaxti);
